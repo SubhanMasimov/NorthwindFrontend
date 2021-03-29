79314130d89aac98faec1a3cc14f5ade5923bfcb
Subhan Masimov <subhanmasimov02@gmail.com>
2021-03-29 17:41:56 +0400

    Camp 19th day

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -10,18 +12,22 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class ProductComponent implements OnInit {
 
-  products: Product[] = []
+  products: Product[] = [];
   dataLoaded = false;
+  filterText = "";
 
-  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
+  constructor(private productService: ProductService, 
+    private activatedRoute: ActivatedRoute,
+    private toastrService : ToastrService,
+    private cartService:CartService) { }
 
   ngOnInit(): void {
 
-    this.activatedRoute.params.subscribe(params=>{
-      if(params["categoryId"]){
+    this.activatedRoute.params.subscribe(params => {
+      if (params["categoryId"]) {
         this.getProductsByCategory(params["categoryId"]);
       }
-      else{
+      else {
         this.getProducts();
       }
     });
@@ -40,6 +46,11 @@ export class ProductComponent implements OnInit {
       this.products = response.data;
       this.dataLoaded = true;
     });
+  }
+
+  addToCart(product: Product) {
+   this.toastrService.success("Səbətə əlavə edildi", product.productName)
+   this.cartService.addToCart(product);
   }
 
 }
